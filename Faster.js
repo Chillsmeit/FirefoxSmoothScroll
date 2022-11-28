c5aa9948817f32d6cd19d884f466f10b9a954626
Chillsmeit <93094077+Chillsmeit@users.noreply.github.com>
2022-11-28 19:01:44 +0000

    Faster Profile
    Faster Profile

--- Faster.js
+++ Faster.js
@@ -1,7 +1,7 @@
 // This profile is close to default but faster
 // COPY THIS AND PUT IN USER.JS
 //*****************************//
-//*SnapFluidFox Faster Profile*//
+//*      Faster Profile       *//
 //*****************************//
 user_pref("general.smoothScroll",                                       true);// Default Settings Below and edited out
 user_pref("general.smoothScroll.msdPhysics.enabled",                    true);// [false]
