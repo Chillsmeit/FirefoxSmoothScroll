bbc9eef21ce34fc21148365f3fb665274f8cffa3
Chillsmeit <93094077+Chillsmeit@users.noreply.github.com>
2022-11-28 18:47:16 +0000

    Default Profile
    Default Profile

--- SnapFluidFox.js
+++ SnapFluidFox.js
@@ -1,7 +1,7 @@
 // COPY THIS AND PUT IN USER.JS
-//**********************//
-//*SnapFluidFox Profile*//
-//**********************//
+//******************************//
+//*SnapFluidFox Default Profile*//
+//******************************//
 user_pref("general.smoothScroll",                                       true);// Default Settings Below and edited out
 user_pref("general.smoothScroll.msdPhysics.enabled",                    true);// [false]
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);// [120] fling
