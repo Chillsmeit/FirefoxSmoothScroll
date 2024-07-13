08ac5c5aaa0aaf4a7cd0beb9831647d88e6a8fe2
Bernardo Melo <chillsmeit@proton.me>
2024-07-13 19:04:19 +0100

    Made some changes, removed all profiles besides snappier which is the default now. Streamlined the user.js

--- Firefox_Tweaks/tweaks.js
+++
@@ -1,6 +0,0 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);//Enables customization (chrome folder)
-user_pref("network.http.referer.XOriginPolicy", 0);//Breaks some Websites if set to 1
-user_pref("privacy.resistFingerprinting", false);//Locks scrolling to 60hz if set to true
-user_pref("ui.key.menuAccessKeyFocuses", false);//Turn off Alt key menu
-user_pref("general.autoScroll", true);//Enables scrollclick scrolling
-user_pref("media.peerconnection.enabled", false);//Disables WebRTC which can leak your real IP even when using a VPN. Some Websites need it
--- user.js
+++ user.js
@@ -1,15 +1,16 @@
-// COPY THIS AND PUT IN USER.JS //
-//******************************//
-//*       Fluid Profile        *//
-//******************************//
+//******************//
+//* Scrolling Start //
+//******************//
 user_pref("general.smoothScroll",                                       true);// Default Settings Below and edited out
 user_pref("general.smoothScroll.msdPhysics.enabled",                    true);// [false]
+user_pref("general.smoothScroll.mouseWheel.migrationPercent",            100);//
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);// [120] fling
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   200);// [1250]
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       250);// [1000]
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           20);// [12]
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",      300);// [2000]
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   550);// [1250]
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       600);// [1000]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           25);// [12]
 user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",     "1.5");// [1.3]
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",      450);// [2000]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio;0",       4);//
 user_pref("general.smoothScroll.currentVelocityWeighting",             "1.0");// ["0.25"]
 user_pref("general.smoothScroll.stopDecelerationWeighting",            "1.0");// ["0.4"]
 // Mousewheel multiply factor - set to false if scrolling is too fast  
@@ -21,7 +22,6 @@ user_pref("mousewheel.system_scroll_override.enabled",                  true);//
 user_pref("mousewheel.default.delta_multiplier_x",                       100);// [100]
 user_pref("mousewheel.default.delta_multiplier_y",                       100);// [100]
 user_pref("mousewheel.default.delta_multiplier_z",                       100);// [100]
-//
 user_pref("apz.allow_zooming",                                          true);// [true]
 user_pref("apz.force_disable_desktop_zooming_scrollbars",              false);// [false]
 user_pref("apz.paint_skipping.enabled",                                 true);// [true]
@@ -43,11 +43,38 @@ user_pref("general.smoothScroll.mouseWheel.durationMinMS",                50);//
 user_pref("layers.async-pan-zoom.enabled",                              true);// [true]
 user_pref("layout.css.scroll-behavior.spring-constant",                "250");// [250]
 user_pref("mousewheel.transaction.timeout",                             1500);// [1500]
-user_pref("mousewheel.acceleration.factor",                                4);// [10]
-user_pref("mousewheel.acceleration.start",                                 1);// [-1]
+user_pref("mousewheel.acceleration.factor",                                3);// [10]
+user_pref("mousewheel.acceleration.start",                                 0);// [-1]
 user_pref("mousewheel.min_line_scroll_amount",                             5);// [5]
 user_pref("toolkit.scrollbox.horizontalScrollDistance",                    5);// [5]
 user_pref("toolkit.scrollbox.verticalScrollDistance",                      3);// [3]
-// If "privacy.resistFingerprinting" is set to "true", it changes the scrolling behaviour and makes it less smooth/clunky
-// Change it to true if you prefer Privacy over scrolling smoothness
-// user_pref("privacy.resistFingerprinting",                              false);//
+//****************//
+//* Scrolling End //
+//****************//
+
+
+//****************//
+//* Tweaks Start  //
+//****************//
+
+// Change it to true if you prefer privacy over scrolling smoothness it makes scrolling less smooth/clunky because it locks it to 60hz
+user_pref("privacy.resistFingerprinting", false);
+
+//Turn off Alt key menu
+user_pref("ui.key.menuAccessKeyFocuses", false);
+
+//Enables scrollclick scrolling
+user_pref("general.autoScroll", true);
+
+//Enables customization (chrome folder)
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+//Breaks some Websites if set to 1
+//user_pref("network.http.referer.XOriginPolicy", 0);//Breaks some Websites if set to 1
+
+//Disables WebRTC which can leak your real IP even when using a VPN. Needed by Websites that use DRM like Spotify, etc
+//user_pref("media.peerconnection.enabled", false);
+
+//**************//
+//* Tweaks End  //
+//**************//
